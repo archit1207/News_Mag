653e73856d021b955202a4646ce478aa3088b1b2
archit1207 <architmishra1207@gmail.com>
2025-11-19 13:42:37 +0530

    Update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/News_Mag/',
   plugins: [react()],
-  base: '/News_Mag',
-})
-
+});
