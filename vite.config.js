6334e9baa196e38ede5e24ffb73ae614dec31c5b
archit1207 <architmishra1207@gmail.com>
2025-11-19 13:50:19 +0530

    fix: added base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/News_Mag/',
+  base: '/News_Mag/',   // IMPORTANT
   plugins: [react()],
-});
+})
